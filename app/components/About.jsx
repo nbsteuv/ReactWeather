aa4b5a7bba31b0ba322b4bb3995f286268fce489
Nick Steuver <nbsteuv@gmail.com>
2016-09-24 11:39:53 -0400

    Added about page with links

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -10,7 +10,15 @@ var React = require('react');
 
 var About = (props) => {
   return(
-    <h2>About</h2>
+    <div>
+      <h1 className="text-center">About</h1>
+      <p>This is a weather application built with React</p>
+      <p>Tools used:</p>
+      <ul>
+        <li><a href="https://facebook.github.io/react">React</a></li>
+        <li><a href="http://openweathermap.org">Open Weather Map API</a></li>
+      </ul>
+    </div>
   )
 };
 
