01d00a739fc593888f7f4cebc04b2955a31cdb7f
Nick Steuver <nbsteuv@gmail.com>
2016-09-26 05:05:14 -0400

    Added propTypes to validate and require message

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,6 +1,15 @@
 var React = require('react');
 
 var ErrorModal = React.createClass({
+  getDefaultProps: function(){
+    return {
+      title: 'Error',
+    }
+  },
+  propTypes: {
+    title: React.PropTypes.string,
+    message: React.PropTypes.string.isRequired,
+  }
   componentDidMount: function(){
     var modal = new Foundation.Reveal($('#error-modal'));
     modal.open();
